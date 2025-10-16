ad8e228281bb19528d269498eede1335111e7e65
Abhinand S <abhinandkarikkakom@gmail.com>
2025-10-16 17:01:38 +0530

    hw2

---
+++ hw2.py
@@ -0,0 +1,7 @@
+para='''Python is a widely used high-level programming language for general-purpose programming, created by Guido van Rossum 
+and first released in 1991. An interpreted language, Python has a design philosophy that emphasizes code readability.'''
+print(len(para));
+print(para[0],para[-1]);
+print(para[0:50]);
+print(para.replace('Python','PYTHON'));
+print(para.lower());
